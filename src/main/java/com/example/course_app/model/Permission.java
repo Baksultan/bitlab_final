0e3ec79c6f55e9f15a2984391640b17437bf5933
Baksultan <amangeldibaksultan@gmail.com>
2023-07-21 12:09:55 +0600

    COMMIT

--- src/main/java/com/example/course_app/model/Permission.java
+++ src/main/java/com/example/course_app/model/Permission.java
@@ -1,27 +1,27 @@
-    package com.example.course_app.model;
+package com.example.course_app.model;
 
-    import jakarta.persistence.*;
-    import lombok.Getter;
-    import lombok.Setter;
-    import org.springframework.security.core.GrantedAuthority;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
-    @Entity
-    @Table(name = "t_permission")
-    @Getter
-    @Setter
-    public class Permission implements GrantedAuthority {
+@Entity
+@Table(name = "t_permission")
+@Getter
+@Setter
+public class Permission implements GrantedAuthority {
 
-        @Id
-        @Column(name = "id")
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column(name = "role")
-        private String role;
+    @Column(name = "role")
+    private String role;
 
-        @Override
-        public String getAuthority() {
-            return this.role;
-        }
-
+    @Override
+    public String getAuthority() {
+        return this.role;
     }
+
+}
